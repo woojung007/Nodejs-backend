3d4b72044b30f37b2fae7337590fca02cadf1d79
woojung007 <eeeeve007@gmail.com>
2022-11-22 23:49:02 +0900

    Day 05 - CORS, GraphQL

--- class/05/05-04-graphql-api-with-apollo-server-phone/index.js
+++ class/05/05-04-graphql-api-with-apollo-server-phone/index.js
@@ -1,5 +1,4 @@
 import { ApolloServer, gql } from "apollo-server";
-import { checkValidationPhone, getToken, sendTokenToSNS } from "./phone.js";
 
 const typeDefs = gql`
   # Return
@@ -18,19 +17,20 @@ const typeDefs = gql`
   }
 
   type Query {
+    # fetchBoards: BoardReturn => 객체 1개를 의미
     fetchBoards: [BoardReturn] # => 배열 안에 객체 여러개를 의미
   }
 
   type Mutation {
     createBoard(writer: String, title: String, contents: String): String
     createBoard2(createBoardInput: CreateBoardInput): String
-    createTokenOfPhone(phone: String): String
   }
 `;
 
 const resolvers = {
   Query: {
     fetchBoards: () => {
+      // 1. 데이터를  조회하는 로직 => DB에 접속해서 데이터 꺼내오기
       const result = [
         {
           number: 1,
@@ -52,18 +52,27 @@ const resolvers = {
         },
       ];
 
+      // 2. 꺼내온 결과 응답 주기
       return result;
     },
   },
 
   Mutation: {
+    // parent - api에서 api를 호출할 때 사용
+    // args - 프론트에서 내용을 받아올 때(Arguments) 사용
     createBoard: (parent, args, context, info) => {
+      // 1. 데이터를  등록하는 로직 => DB에 접속해서 데이터 저장하기
       console.log("args", args);
+
+      // 2. 저장 결과 알려주기!!
       return "등록 성공!!";
     },
 
     createBoard2: (_, args) => {
+      // 1. 데이터를  등록하는 로직 => DB에 접속해서 데이터 저장하기
       console.log("args", args);
+
+      // 2. 저장 결과 알려주기!!
       return "등록 성공!!";
     },
 
